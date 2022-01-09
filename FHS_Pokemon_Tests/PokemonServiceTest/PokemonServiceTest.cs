22284175cdd882632d15472844b97debf8790521
fhs2020 <flavioh007@gmail.com>
2022-01-08 23:16:48 -0300

    adding XUnit test

---
+++ FHS_Pokemon_Tests/PokemonServiceTest/PokemonServiceTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+
+using Moq;
+
+using Pokemon.API.Context;
+using Pokemon.API.Domain;
+using Pokemon.API.Interfaces;
+using Pokemon.API.Services;
+
+using Xunit;
+
+namespace FHS_Pokemon_Tests
+{
+    public class PokemonServiceTest
+    {
+
+        public PokemonServiceTest()
+        {
+
+        }
+ 
+
+        [Fact]
+        public async Task ListaPokemons()
+        {
+            // Arrange
+
+
+            var optionsBuilder = new DbContextOptionsBuilder<PokemonContext>();
+            optionsBuilder.UseSqlite("name=ConnectionStrings:DefaultConnection");
+
+            var mockSet = new Mock<DbSet<PokemonModel>>();
+
+            var mockContext = new Mock<PokemonContext>(optionsBuilder);
+
+           // mockContext.Setup(m => m.Pokemon).Returns(mockSet.Object);
+
+            using (var context = new PokemonContext(optionsBuilder.Options))
+            {
+                var service = new PokemonServices(context);
+                var list = await service.GetPokemones();
+                Assert.NotNull(list);
+            }
+
+            // Act
+            //var result = await pokemonService.GetPokemones();
+
+            // Asser
+            //Assert.True(result.results.Any());
+
+        }
+ 
+    }
+}
---
+++ Pokemon_FH.Tests/PokemonApiTests/PokemonServiceTestes.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using Pokemon.API.Context;
+using Pokemon.API.Helper;
+using Pokemon.API.Interfaces;
+using Pokemon.API.Services;
+
+using System.Linq;
+using System.Threading.Tasks;
+
+using Xunit;
+
+
+
+
+namespace Pokemon_FH.Tests.PokemonApiTests
+{
+    [Collection("IPokemon")]
+    public class PokemonServiceTestes
+    {
+        private IPokemon _pokemonService;
+        PokeAPI _api = new PokeAPI();
+
+        public PokemonServiceTestes()
+        {
+            var options = new DbContextOptionsBuilder<PokemonContext>()
+                .UseInMemoryDatabase(databaseName: "FakeDatabase")
+                .Options;
+
+            var dbContext = new PokemonContext(options);
+
+            _pokemonService = new PokemonServices(dbContext);
+
+        }
+
+        [Fact]
+        public async Task ListarPokemons()
+        {
+            // Arrange
+            //var pokemonModel = new Moc<PokemonModel>();
+
+            // Act
+            var result = await _pokemonService.GetPokemones();
+
+            // Asser
+            Assert.True(result.results.Any());
+
+
+
+
+        }
+    }
+}
